e249136dff6dcfaa0417a57a138b00926069c7d5
Luan Nico <luanpotter28@hotmail.com>
2015-01-14 21:57:57 -0200

    New List API for using indexes when possible

--- src/main/java/xyz/luan/reflection/tclass/ListClass.java
+++ src/main/java/xyz/luan/reflection/tclass/ListClass.java
@@ -3,6 +3,7 @@ package xyz.luan.reflection.tclass;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Array;
 import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
@@ -22,6 +23,25 @@ public class ListClass<T> extends TypedClass<T> {
 	@Override
 	public boolean isList() {
 		return true;
+	}
+
+	public void forEach(Object object, BiConsumer<Integer, Object> consumer) {
+		if (ref.isArray()) {
+			arrayForEach(object, consumer);
+		} else {
+			listForEach(object, consumer);
+		}
+	}
+
+	private void listForEach(Object object, BiConsumer<Integer, Object> consumer) {
+		final AtomicInteger index = new AtomicInteger(-1);
+		Collection.class.cast(object).forEach((el) -> consumer.accept(index.incrementAndGet(), el));
+	}
+
+	private void arrayForEach(Object object, BiConsumer<Integer, Object> consumer) {
+		for (int i = 0; i < Array.getLength(object); i++) {
+			consumer.accept(i, Array.get(object, i));
+		}
 	}
 
 	public void forEach(Object object, Consumer<Object> consumer) {
